616283e2921fadba7288cf95fe4252ca7f7c8a5e
Jonathan Gryn <jonathangryn@Jonathans-MBP.home>
2017-09-16 20:05:54 -0700

    added HTML routes

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -2,4 +2,24 @@
 // Created: Sep. 15, 2017 12:05 AM
 // Author: Jonathan Gryn
 // Revisions: Jon (9/15/17) - Started htmlRoutes.js
+//            Jon (9/16/17) - Added HTML routes
 */
+
+// Pull in required dependencies
+var path = require('path');
+
+// Export HTML routes
+module.exports = function(app) {
+
+    // console.log('___ENTER htmlRoutes.js___');
+
+    // Home page
+    app.get('/', function(req, res) {
+        res.sendFile(path.join(__dirname, '../public/home.html'));
+    });
+
+    // Survey page
+    app.get('/survey', function(req, res) {
+        res.sendFile(path.join(__dirname, '../public/survey.html'));
+    });
+};
